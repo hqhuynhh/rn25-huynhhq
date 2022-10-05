a01a1f664e58c6753667a4de8384b99a0f02e00d
hqhuynh <hqhuynhbk@gmail.com>
2022-10-05 21:07:18 +0700

    hoc510

--- rn25-huynh/src/App.tsx
+++ rn25-huynh/src/App.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+
+function App(): JSX.Element {
   return (
-    <div className="App">
-      Hello world 12356
+    <>
+    <h1 className="App"> The Pulpit Rock</h1>
+    <br />
+    <div className="container text-center">
+      <div className="row align-items-start">
+        <div className="col">
+          Dong 1
+        </div>
+        <div className="col">
+          One of three columns
+        </div>
+        <div className="col">
+          WHAT?
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col">
+          One of three columns
+        </div>
+        <div className="col">
+          One of three columns
+        </div>
+        <div className="col">
+          WHERE?
+        </div>
+      </div>
+      <div className="row align-items-end">
+        <div className="col">
+          One of three columns
+        </div>
+        <div className="col">
+          One of three columns
+        </div>
+        <div className="col">
+          WHEN?
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
